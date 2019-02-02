637982b32f4f8eec7e423b031584c55f687cdfe5
DESKTOP-K7MRA8M\albwe <aw@gerecht.biz>
2019-02-02 17:05:36 +0100

    Tags öffentlich sichtbar

--- php/getstellen.php
+++ php/getstellen.php
@@ -9,6 +9,14 @@ $markers=array();
 $query= $mysql->query("SELECT stellen.*, b.supported FROM stellen LEFT JOIN (SELECT fk_stelle AS id, COUNT(*) AS supported FROM votes GROUP BY fk_stelle ORDER BY supported DESC) AS b ON b.id=stellen.id WHERE stellen.published=1 AND stellen.declined=0 ORDER BY b.supported DESC;");
 $i=0;
 while($t=$query->fetch_assoc()) {
+  $tagq = $mysql->query("SELECT * FROM tags WHERE fk_stelle=".$t['id'].";");
+  $tags = array();
+  $j=0;
+  while($tg=$tagq->fetch_assoc()) {
+    $tags[$j]=$tg;
+    ++$j;
+  }
+  $t['tags']=$tags;
   $markers[$i]=$t;
   ++$i;
 }
